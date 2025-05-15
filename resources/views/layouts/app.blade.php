29e07f04a1c7e9dfe497005fd469d7c253704118
rafif <rafifidnsolo007@gmail.com>
2025-05-15 14:06:35 +0700

    ngambil user dari rafif

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
             <a href="{{ route('admin.users') }}"
                 class="flex items-center space-x-3 px-4 py-3 rounded-lg 
                 {{ request()->routeIs('admin.users') ? 'bg-white bg-opacity-20' : 'hover:bg-white hover:bg-opacity-10' }}">
-                <i class="fas fa-user"></i>
+                <i class="fas fa-users"></i>
                 <span>User List</span>
             </a>
              {{-- orders --}}
